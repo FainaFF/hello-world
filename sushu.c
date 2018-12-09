fb4f6e44e21a2dae615ab74ccd5a05826a92b5bd
FainaFF <13152798801@163.com>
2018-12-09 22:07:14 +0800

    sushu.c add commit

--- sushu.c
+++ sushu.c
@@ -3,15 +3,20 @@ int main()
 {
  int i,j,n;
  scanf("%d",&n);
- for(i=2;i<n;i++)
- {
-  j=n%i;
-  if(j==0)
-  break;	
- }
- if(j==0)
+ if(n==0||n==1)
  printf("%d不是素数",n);
  else
- printf("%d是素数",n);
- return 0;	 
+ {
+  for(i=2;i<n;i++)
+  {
+   j=n%i;
+   if(j==0)
+   break;	
+  }
+  if(j==0)
+  printf("%d不是素数",n);
+  else
+  printf("%d是素数",n);
+ }
+return 0;	 
 }
